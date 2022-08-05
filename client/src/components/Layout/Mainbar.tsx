8bceb389a795069eebf63474bd95beaff8f30a7a
camilo kosniowski <camilo.kosniowski@unosquare.com>
2022-08-05 11:38:26 -0500

    added i18n to localStorange, an fixed eslint issues

--- client/src/components/Layout/Mainbar.tsx
+++ client/src/components/Layout/Mainbar.tsx
@@ -1,42 +1,52 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Button } from '../UI/Button';
-import styles from '../Layout/Mainbar.module.sass';
 import { useTranslation } from 'react-i18next';
-import esFlag from '../UI/assets/Flags/es.svg'
-import usFlag from '../UI/assets/Flags/us.svg'
+import { Button } from '../UI/Button';
+import styles from './Mainbar.module.sass';
+import esFlag from '../UI/assets/Flags/es.svg';
+import usFlag from '../UI/assets/Flags/us.svg';
 
 export function Mainbar() {
-  const [t, i18n] = useTranslation('global');
-  const navigate = useNavigate();
-  const handleLogin = () => {
-    console.log('im clicking');
-    navigate('./login', { replace: true });
-  };
-  return (
-    <nav className={styles.mainbar}>
-      <div className={styles.logo}>ReserveFacil.com</div>
-      <ul className={styles['items-list']}>
-        <li>
-          <a href='/'>{t('main-home.main-home')}</a>
-        </li>
-        <li>
-          <a href='/about'>{t('main-about.main-about')}</a>
-        </li>
-        <li>
-          <a href='/contact'>{t('main-contact.main-contact')}</a>
-        </li>
-        <li>
-        <div className='dropdown'>
-          {/* <span>lang</span> */}
-          <div className='dropdown-content'>
-            <button onClick={() => i18n.changeLanguage('es')}>ES <img  className={styles.img}src={esFlag} alt="es"/></button>
-            <button onClick={() => i18n.changeLanguage('en')}>EN <img  className={styles.img}src={usFlag} alt="es"/></button>
-          </div>
-          </div>
-        </li>
-      </ul>
-      <Button title={t('login.login')} onClick={handleLogin} />
-    </nav>
-  );
+	const [t, i18n] = useTranslation('global');
+	const navigate = useNavigate();
+	const handleLogin = () => {
+		console.log('im clicking');
+		navigate('./login', { replace: true });
+	};
+	const handleChangleLng = (lng: any) => {
+		i18n.changeLanguage(lng);
+		localStorage.setItem('lng', lng);
+	};
+
+	return (
+		<nav className={styles.mainbar}>
+			<div className={styles.logo}>ReserveFacil.com</div>
+			<ul className={styles['items-list']}>
+				<li>
+					<a href='/'>{t('main-home.main-home')}</a>
+				</li>
+				<li>
+					<a href='/about'>{t('main-about.main-about')}</a>
+				</li>
+				<li>
+					<a href='/contact'>{t('main-contact.main-contact')}</a>
+				</li>
+				<li>
+					<div className='dropdown'>
+						{/* <span>lang</span> */}
+						<div className='dropdown-content'>
+							<label onClick={() => handleChangleLng('es')}>
+								ES <img className={styles.img} src={esFlag} alt='es' />
+							</label>
+							&nbsp;&nbsp;&nbsp;
+							<label onClick={() => handleChangleLng('en')}>
+								EN <img className={styles.img} src={usFlag} alt='es' />
+							</label>
+						</div>
+					</div>
+				</li>
+			</ul>
+			<Button title={t('login.login')} onClick={handleLogin} />
+		</nav>
+	);
 }
