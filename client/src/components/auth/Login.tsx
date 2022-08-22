20723d18b697a1ab7bc332a971a5fce358f35c18
FelipeRUnosquare <felipe.ramirez@unosquare.com>
2022-08-22 12:49:22 -0500

    Redux set up on front project

--- client/src/components/auth/Login.tsx
+++ client/src/components/auth/Login.tsx
@@ -1,18 +1,29 @@
+import { Button } from "@mui/material";
 import React, { useState } from "react";
+import { useAppDispatch } from "../../hook";
+import { authActions } from "../../store/auth";
 import style from "./login.module.sass";
+import { useTranslation } from "react-i18next";
 
 export function Login() {
+  const dispatch = useAppDispatch();
+  const [t, i18n] = useTranslation("global");
   const [error, setError] = useState<
     string | { message: string; code: number }
   >();
 
-  setError({ message: "Error", code: 1 });
+  const submitLoginForm = () => {
+    dispatch(authActions.login());
+  };
 
   return (
     <div className={style.container}>
       <form className={style.form}>
+        <label htmlFor="username">{t("login.username")}</label>
         <input type="text" />
+        <label>{t("login.password")}</label>
         <input type="text" />
+        <Button onClick={submitLoginForm}>{t("login.login")}</Button>
       </form>
     </div>
   );
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,53 +1,57 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import React, { Component } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 // import i18n (needs to be bundled ;))
-import { I18nextProvider } from 'react-i18next';
-import i18next from 'i18next';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { I18nextProvider } from "react-i18next";
+import i18next from "i18next";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-import global_es from './Translations/es/global.json';
-import global_en from './Translations/en/global.json';
+import global_es from "./Translations/es/global.json";
+import global_en from "./Translations/en/global.json";
 
 import {
-	ApolloClient,
-	InMemoryCache,
-	ApolloProvider,
-	gql
-} from '@apollo/client';
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
+import { Provider } from "react-redux";
+import store from "../src/store";
 const client = new ApolloClient({
-	uri: 'https://flyby-gateway.herokuapp.com/',
-	cache: new InMemoryCache()
+  uri: "https://flyby-gateway.herokuapp.com/",
+  cache: new InMemoryCache(),
 });
 
 i18next.init({
-	interpolation: { escapeValue: false },
-	lng: localStorage.getItem('lng') || 'en',
-	resources: {
-		es: {
-			global: global_es
-		},
-		en: {
-			global: global_en
-		}
-	}
+  interpolation: { escapeValue: false },
+  lng: localStorage.getItem("lng") || "en",
+  resources: {
+    es: {
+      global: global_es,
+    },
+    en: {
+      global: global_en,
+    },
+  },
 });
 
 const root = ReactDOM.createRoot(
-	document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
-	<ApolloProvider client={client}>
-		<React.StrictMode>
-			<I18nextProvider i18n={i18next}>
-				<BrowserRouter>
-					<App />
-				</BrowserRouter>
-			</I18nextProvider>
-		</React.StrictMode>
-	</ApolloProvider>
+  <ApolloProvider client={client}>
+    <Provider store={store}>
+      <React.StrictMode>
+        <I18nextProvider i18n={i18next}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </I18nextProvider>
+      </React.StrictMode>
+    </Provider>
+  </ApolloProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
